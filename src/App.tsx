62b10a7aee53150b20df8973031439d54f7e600f
Eudes Junior <juniorsousa_343@hotmail.com>
2023-11-03 18:30:00 -0300

    feat: cria arquivos de configuração do tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Heading, useColorMode } from "@chakra-ui/react";
 
 function App() {
-  return <Heading>Vite + React</Heading>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Heading>Vite + React</Heading>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+    </>
+  );
 }
 
 export default App;
